ddd2fc19c72446a8d9313037b52a7d3f488be63a
DahyeChung <lewachung@gmail.com>
2023-05-19 14:34:26 +0900

    Add exit event

--- 2DspriteDungeon/2DspriteDungeon/GameCore.cpp
+++ 2DspriteDungeon/2DspriteDungeon/GameCore.cpp
@@ -4,19 +4,37 @@ namespace DungeonGame
 {
 	void InitializeGame(PlayerState& playerState, WorldState& worldState)
 	{
-
+		playerState.HasFinishedGame = false;
 	}
 	void GetInput(PlayerState& playerState, WorldState& worldState)
 	{
+		SDL_Event e = {};
+		while (SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_QUIT)
+				playerState.HasFinishedGame = true;
+			else if (e.type == SDL_KEYDOWN)
+			{	
+				auto keyCode = e.key.keysym.sym;
+				switch (keyCode)
+				{
+				case SDLK_ESCAPE:
+					playerState.HasFinishedGame = true;
+					break;
+				}
 
+			}
+		}
 	}
 	void UpdateGame(PlayerState& playerState, WorldState& worldState)
 	{
 
 	}
 	void RenderGame(SDL_Renderer* pRenderer, const PlayerState& playerState, const WorldState& worldState)
-	{
-
+	{	
+		SDL_RenderClear(pRenderer);
+		//Render Commands go here ! 
+		SDL_RenderPresent(pRenderer);
 	}
 	void CleanupGame(PlayerState& playerState, WorldState& worldState)
 	{
--- 2DspriteDungeon/2DspriteDungeon/main.cpp
+++ 2DspriteDungeon/2DspriteDungeon/main.cpp
@@ -32,7 +32,7 @@ int main()
 
     }
 
-    CleanupGame();
+    CleanupGame(playerState, worldState);
 
        
 
